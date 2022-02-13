fb268d6135ed802157e92b650f601687c82ee316
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-13 02:28:22 +0200

    creating notification tableView

--- Social_App/Controllers/NotificationViewController.swift
+++ Social_App/Controllers/NotificationViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+// to set navigartion controller item without set title for the tabBar item
+        navigationController?.navigationBar.topItem?.title = "Notification"
+/*
+        -this code set title for the navigation controller and the tabBar item
+        i don't know why :)
+       // title = "Notification"
+      //  tabBarItem.title = " "  // NOT WORKING
+*/
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "notificationCell") as! notificationTableViewCell
+       
+        return cell
+    }
+    
+    
     
 
    
---
+++ Social_App/Controllers/notificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  notificationTableViewCell.swift
+//  Social_App
+//
+//  Created by Eslam Ali  on 13/02/2022.
+//
+
+import UIKit
+
+class notificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
